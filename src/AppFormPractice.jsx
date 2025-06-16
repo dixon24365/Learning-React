185ce6a85f07169183a6647261423c6bbbf630cf
dixon24365 <tuedin.james@gmail.com>
2025-06-16 16:23:44 +0300

    Did radio button practice

--- src/AppFormPractice.jsx
+++ src/AppFormPractice.jsx
@@ -2,32 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './AppFormPractice.css'
 
-/**
- * Challenge: add another label and input for the password field
- */
-
 function signUp(formData){
     const email= formData.get("email")
     const password= formData.get("password")
+    const employmentStatus= formData.get("employmentStatus")
     console.log(email)
     console.log(password)
+    console.log(employmentStatus)
 }
 
 export default function AppFormPractice() {
   return (
-    <section>
+  <section>
       <h1>Signup form</h1>
       <form action={signUp}>
+
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
 
         <label htmlFor="password">Password:</label>
-        <input id="password" type="passwoef" name="password" />
-        <br />
-        
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="umemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time"/>
+            Full-time
+        </label>
+        </fieldset>
+
+
+
         <button>Submit</button>
-        
+
       </form>
     </section>
   )
