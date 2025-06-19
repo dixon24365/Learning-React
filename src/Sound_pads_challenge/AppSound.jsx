32d9af73f47ab5777d720c60e9771e56652571b7
dixon24365 <tuedin.james@gmail.com>
2025-06-19 14:53:20 +0300

    random commit

---
+++ src/Sound_pads_challenge/AppSound.jsx
@@ -0,0 +1,11 @@
+import pads from "./pads"
+
+export default function AppSound(){
+    return(
+          <main>
+            <div className="pad-container">
+                {/* <button>s go here */}
+            </div>
+        </main>
+    )
+}
